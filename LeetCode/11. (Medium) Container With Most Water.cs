9c08c524fed35ae4b10bff705213c29257d05c65
Rooney <ntut106820037@gmail.com>
2023-09-20 01:48:41 +0800

    Finished 11, 1476, 1828

---
+++ LeetCode/11. (Medium) Container With Most Water.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Solution11
+    {
+        public int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int max = 0;
+            int area = 0;
+
+            while (left != right)
+            {
+                area = (right - left) * Math.Min(height[left], height[right]);
+                if (max < area)
+                    max = area;
+
+                if (height[left] > height[right])
+                    right--;
+                else
+                    left++;
+            }
+
+
+            return max;
+        }
+    }
+}
---
+++ LeetCode/1828. (Medium)  Queries On Number of Points Inside a Circle.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading;
+
+public class Solution1828
+{
+    public int[] CountPoints(int[][] points, int[][] queries)
+    {
+        int[] count = new int[queries.Length];
+        int index = 0;
+
+        foreach (int[] circle in queries)
+        {
+            int rr = circle[2] * circle[2];
+            foreach(int[] p in points)
+            {
+                count[index] += (p[0] - circle[0]) * (p[0] - circle[0]) + (p[1] - circle[1]) * (p[1] - circle[1]) <= rr ? 1: 0;
+            }
+            index++;
+        }
+
+
+        return count;
+    }
+
+    public static void Main()
+    {
+        int[][] points = new int[4][];
+        points[0] = new int[] { 1, 3 };
+        points[1] = new int[] { 3, 3 };
+        points[2] = new int[] { 5, 3 };
+        points[3] = new int[] { 2, 2 };
+
+        int[][] queries = new int[][]{  new int[] { 2, 3, 1 }, 
+                                        new int[] { 4, 3, 1 }, 
+                                        new int[] { 1, 1, 2 } };
+
+        int[] arr = new int[3] { 1, 2, 3};
+        Solution1828 so = new Solution1828();
+        arr = so.CountPoints(points, queries);
+        foreach(int i in arr)
+        {
+            Console.Write(i.ToString() + ", ");
+        }
+        Thread.Sleep(1000);
+    }
+}
