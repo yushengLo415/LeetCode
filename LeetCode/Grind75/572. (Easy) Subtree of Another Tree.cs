f73b7ae6ff905ebdf4eead1b084f5442239adff4
yushengLuo <ntut106820037@gmail.com>
2023-10-29 16:01:42 +0800

    Grind75 573 977

--- LeetCode/Grind75/572. (Easy) Subtree of Another Tree.cs
+++ LeetCode/Grind75/572. (Easy) Subtree of Another Tree.cs
@@ -11,6 +11,25 @@ namespace LeetCode.Grind75
         public bool IsSubtree(TreeNode root, TreeNode subRoot)
         {
 
+            if (root == null)
+                return false;
+
+            if (root.val == subRoot.val && CheckDFS(root, subRoot))
+                return true;
+
+            return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+        }
+
+        public bool CheckDFS(TreeNode root, TreeNode subRoot)
+        {
+            if (root == null && subRoot == null)
+                return true;
+
+            if (root == null || subRoot == null || root.val != subRoot.val)
+                return false;
+
+            return CheckDFS(root.left, subRoot.left) && CheckDFS(root.right, subRoot.right);
+
         }
     }
 }
---
+++ LeetCode/Grind75/977. (Easy) Squares of a Sorted Array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Grind75
+{
+    public class Solution977
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > -1)
+                    break;
+                nums[i] *= -1;
+            }
+
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+                nums[i] *= nums[i];
+
+            return nums;
+        }
+    }
+}
