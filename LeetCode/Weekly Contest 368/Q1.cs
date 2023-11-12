e516cabff6c20efd84d8e136087c54710d679c98
yushengLuo <ntut106820037@gmail.com>
2023-11-12 18:37:17 +0800

    Week contest 371: 2932, 2933

--- LeetCode/Weekly Contest 368/Q1.cs
+++ LeetCode/Weekly Contest 368/Q1.cs
@@ -10,7 +10,7 @@ namespace LeetCode.Weekly_Contest_368
     {
         public class SolutionQ1
         {
-            public int MinGroupsForValidAssignment(int[] nums)
+            /*public int MinGroupsForValidAssignment(int[] nums)
             {
 
                 Dictionary<int, int> dic = new Dictionary<int, int>();
@@ -42,16 +42,75 @@ namespace LeetCode.Weekly_Contest_368
                 }
 
                 return sum;
+            }*/
+            public int MinOperations(int[] nums1, int[] nums2)
+            {
+                int n = nums1.Length;
+
+                int last1 = nums1[n - 1];
+                int last2 = nums2[n - 1];
+                int ans = 0;
+                int temp = 0;
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    bool swap = false;
+
+                    if (nums1[i] > last1)
+                    {
+                        if (nums1[i] > last2)
+                            return -1;
+                        else
+                        {
+                            ans += 1;
+                            swap = true;
+                        }
+                    }
+
+                    if (swap)
+                    {
+                        if (nums1[i] > last2)
+                            return -1;
+                    }
+                    else
+                    {
+                        if (nums2[i] > last2)
+                        {
+                            if (nums1[i] > last2)
+                                return -1;
+                            else
+                            {
+                                ans += 1;
+                                swap = true;
+                            }
+                        }
+
+                    }
+
+                    if (nums1[i] <= last1 && nums1[i] <= last2 && nums2[i] <= last1 && nums2[i] <= last2)
+                        temp += 1;
+
+
+                }
+
+                if (ans >= n - ans)
+                {
+                    ans = n - ans - temp;
+                }
+
+                return ans;
             }
 
-           /* static int Main()
+
+            static int Main()
             {
                 SolutionQ1 sol = new SolutionQ1();
-                int[] arr = new int[] { 1, 1, 3, 3, 1, 1, 2, 2, 3, 1, 3, 2 };
-                Console.WriteLine(sol.MinGroupsForValidAssignment(arr));
+                int[] arr1 = new int[] { 8, 6, 6, 6, 7, 8 };
+                int[] arr2 = new int[] { 5, 8, 8, 8, 7, 7 };
+                Console.WriteLine(sol.MinOperations(arr1, arr2));
                 Console.ReadKey();
                 return 0;
-            }*/
+            }
         }
     }
 }
