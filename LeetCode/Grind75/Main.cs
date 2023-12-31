e7c1451405915dbefc8685ba59a044be2ab35889
yushengLuo <ntut106820037@gmail.com>
2023-12-31 12:05:19 +0800

    Weekly Context 378

--- LeetCode/Grind75/Main.cs
+++ LeetCode/Grind75/Main.cs
@@ -1,20 +1,37 @@
-﻿using System;
+﻿using LeetCode.Weekly_Contest_378;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Diagnostics;
+
 namespace LeetCode.Grind75
 {
     class SolutionMain
     {
-        static int Main()
+       /* static int Main()
         {
             Solution sol = new Solution973();
             sol.RunScript();
             Console.ReadKey();
             return 0;
 
+        }*/
+
+        static int Main()
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            Q3 q3 = new Q3();
+            Console.WriteLine(q3.MaximumLength("edvjicilcvrzngyffduvzniqksazazxuibbhnjzzlizmcesiyhxkqwahszqspghitkhgdfqkcmggtaozzzzzzzzzzzzzzzzzzzeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeekkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooowwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwalzxulgaclfrelrvknbcshnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnleosbwvtulqbjjuqcljiemtdzorsdywglafpyttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttzfvqqhxx"));
+            
+            stopWatch.Stop(); //停止Stopwatch
+            Console.WriteLine(stopWatch.ElapsedMilliseconds);
+
+            Console.ReadKey();
+            return 0;
         }
     }
 }
---
+++ LeetCode/Weekly Contest 378/10032. (Medium) Find Longest Special Substring That Occurs Thrice II.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Weekly_Contest_378
+{
+    class Q3
+    {
+        public int MaximumLength(string s)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            for (int p = s.Length; p >= 1; p--)
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (i + p <= s.Length)
+                    {
+                        string substring = s.Substring(i, p);
+
+                        if (!IsSpecial(substring))
+                            continue;
+
+                        if (!dict.ContainsKey(substring))
+                            dict[substring] = 0;
+
+                        dict[substring]++;
+
+                        if (dict[substring] >= 3)
+                            return substring.Length;
+                    }
+                }
+
+
+            //abcaba
+            return -1;
+        }
+
+        private bool IsSpecial(string s)
+        {
+            if (s.Length < 1)
+                return false;
+
+            for (int i = 0;  i < s.Length - 1; i++)
+            {
+                if (s[i] != s[i + 1])
+                    return false;
+            }
+
+            return true;
+        }
+
+    }
+}
